f8ea51eb7a881b96ab0eb562d12ead5f56a230f2
frank Freeman <frankfigao@gmail.com>
2024-10-18 17:05:52 -0600

    chore: adding shadcn

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,10 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+          edu
+          {children}
+        </main>
       </body>
     </html>
   );
